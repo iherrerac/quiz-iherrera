963ab8982ff46ca80a3bccf0ed08507b33c77fe4
Israel Herrera Cabello <isra.herrera@hotmail.com>
2015-08-02 17:57:04 +0200

    Eliminar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -110,6 +110,14 @@ exports.update = function(req, res) {
   ); 
 
 };
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then(function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
+
 /*//GET/quizes/
 exports.index= function(req,res){
     models.Quiz.findAll().then(function(quizes){
