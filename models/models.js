8ee266108ecfc500d6bb380e4dd7ca32ca895f38
Israel Herrera Cabello <isra.herrera@hotmail.com>
2015-08-08 19:43:19 +0200

    Mod9: Crear comentarios

--- models/models.js
+++ models/models.js
@@ -27,12 +27,21 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
-//importar definicion de la tabla quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+// Importar definicion de la tabla  Quiz en quiz.js
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definicion de la tabla Comment en comment.js
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 
-exports.Quiz = Quiz;//exportar la definicion de la tabla quiz 
-
+exports.Quiz = Quiz;//exportar tabla quiz 
+exports.Comment = Comment;
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
