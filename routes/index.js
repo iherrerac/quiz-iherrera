6def405e5f4750ebe2d016e2333c98e5ecea6904
Israel Herrera Cabello <isra.herrera@hotmail.com>
2015-08-09 01:45:08 +0200

    Mod 9: Login

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require ('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -17,6 +18,11 @@ router.param('quizId', quizController.load); // autoload :quizId
 router.get('/author', function(req, res, next) {
   res.render('author.ejs', { nombre: 'Israel Herrera', title: 'Créditos', errors: [] });
 });
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new); 	   // formulario login
+router.post('/login', sessionController.create);   // crear sesión
+router.get('/logout', sessionController.destroy);  // destruir session
 
 // Definición de rutas de /quizes
 router.get('/quizes', 					   quizController.index);
